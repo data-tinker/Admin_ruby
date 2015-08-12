16af71024da274121a4faae727f47f6d4decd374
monkeybaza <alex.martishin@yandex.ru>
2015-08-12 16:25:09 +0300

    now all functions use pipelines

---
+++ task1/functions_pipes.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+servers = YAML::load(File.open('servers.yml'))
+
+
+def find_ssd_only_ids(servers)
+  servers.select{|server| server[:storage].select{|server_storage| server_storage[:type] != :ssd}.empty?}.map{|server| server[:id]}
+end
+
+
+def find_ids_with_ram_over_100(servers)
+  servers.select{|server| server[:ram].reduce(&:+) > 100}.map{|server| server[:id]}
+end
+
+
+def find_ssd_volume_per_server(servers)
+  servers.map do |server|
+    {server[:id] => server[:storage].select{|storage| storage[:type] == :ssd}.reduce(0){|acc, storage| acc + storage[:size]}}
+  end
+end
+
+
+def sorted_ids_by_cpu_frequency(servers)
+  servers.sort_by {|server| server[:cpu][0][:frequency]}.map{|server| server[:id]}
+end
+
+print "#{find_ssd_only_ids(servers)}\n"
