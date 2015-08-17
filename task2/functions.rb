1719c42266aac2cbcc23c6fef31edfc92fff33c7
monkeybaza <alex.martishin@yandex.ru>
2015-08-17 08:42:20 +0300

    task 2 solved

--- task2/functions.rb
+++ task2/functions.rb
@@ -2,30 +2,31 @@
 
 require 'minitest/autorun'
 
+
 class BlocksTest < Minitest::Test
 
   def test_even_map
     input = [11, -2, 32, 14, -5, 26]
     expected = [22, 64, -10]
-    assert_equal(expected, input.even_map { |item| item * 2 })
+    assert_equal(expected, input.even_map{|item| item * 2})
+  end
+
+  def test_even_reduce
+    input = [11, -2, 32, 14, -5, 26]
+    expected = [22, 64, -10]
+    assert_equal(expected, input.even_reduce(0){|acc, item| acc + item})
   end
 
   def test_even_reduce
     input = [11, -2, 32, 14, -5, 26]
     expected = 38
-    assert_equal(expected, input.even_reduce(0) { |acc, item| acc + item })
-  end
-
-  def test_even_reduce_arg
-    input = [11, -2, 32, 14, -5, 26]
-    expected = 38
-    assert_equal(expected, input.even_reduce_arg(0, lambda { |acc, item| acc + item }))
+    assert_equal(expected, input.even_reduce_arg(0 ,lambda{|acc, item| acc + item}))
   end
 
   def test_map_with_siblings
     input = [1, 2, 3]
     expected = [[nil, 1, 2], [1, 2, 3], [2, 3, nil]]
-    assert_equal(expected, input.map_with_siblings { |prev, current, nextt| [prev, current, nextt] })
+    assert_equal(expected, input.map_with_siblings{|prev ,current, nextt|[prev, current, nextt]})
   end
 
 end
@@ -70,6 +71,9 @@ class Array
       end
       current = self[i]
       nextt = self[i + 1]
+#      if i >= self.size
+#        nextt = nil
+#      end
       result.push(yield(prev, current, nextt))
     end
     return result
